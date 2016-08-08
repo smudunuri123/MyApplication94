c35e82c0e151d71599ddfa3744361101b9526f0b
smudunuri123 <smudunuri@google.com>
2016-08-08 15:49:05 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/smudunuri/myapplication/InsT.java
@@ -0,0 +1,13 @@
+package com.example.smudunuri.myapplication;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by smudunuri on 8/8/2016.
+ */
+
+public class InsT extends InstrumentationTestCase {
+    public void testSampleTest(){
+        assertEquals(true, true);
+    }
+}
